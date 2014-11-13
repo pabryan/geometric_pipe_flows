6d3398bd15ce7884e3cd1ebb61e9e80a4f26e382
Paul Bryan <pabryan@gmail.com>
2014-11-13 13:28:01 -0800

    Wrote publishing of auxillary files such as TeX files

--- publish.el
+++ publish.el
@@ -1,23 +1,35 @@
 (setq pb/geometric_pipe_flows-org-publish-project-alist
-      '(("geometric_pipe_flows"
-	:base-directory "~/working/academic/research/geometric_pipe_flows"
-	:publishing-directory "~/publish/geometric_pipe_flows"
-	:base-extension "org"
-	:recursive t
-	:publishing-function org-latex-publish-to-latex
-	:export-with-tags nil
-	:headline-levels 10
-	:with-toc nil
-	:section-numbers t
-;	:sub-superscript nil
-	:todo-keywords nil
-	:author nil
-	:creator-info nil
-	:table-auto-headline t
-	:style-include-default t
-	:timestamp f
-	:exclude-tags ("noexport")
-	:auto-preamble t)))
+      '(
+	("geometric_pipe_flows-org"
+	 :base-directory "~/working/academic/research/geometric_pipe_flows"
+	 :publishing-directory "~/publish/geometric_pipe_flows"
+	 :base-extension "org"
+	 :recursive t
+	 :publishing-function org-latex-publish-to-latex
+	 :export-with-tags nil
+	 :headline-levels 10
+	 :with-toc nil
+	 :section-numbers t
+					;	:sub-superscript nil
+	 :todo-keywords nil
+	 :author nil
+	 :creator-info nil
+	 :table-auto-headline t
+	 :style-include-default t
+	 :timestamp f
+	 :exclude-tags ("noexport")
+	 :auto-preamble t)
+
+	("geometric_pipe_flows-static"
+	 :base-directory "~/working/academic/research/geometric_pipe_flows"
+	 :publishing-directory "~/publish/geometric_pipe_flows"
+	 :recursive t
+	 :base-extension "tex\\|pdf"
+	 :publishing-function org-publish-attachment
+	 )
+
+        ("geometric_pipe_flows" :components ("geometric_pipe_flows-org" "geometric_pipe_flows-static"))
+	))
 
 
 (setq org-publish-project-alist pb/geometric_pipe_flows-org-publish-project-alist)
