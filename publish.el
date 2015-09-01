cf101b6f662148cf053468811013e675f17c2680
Paul Bryan <pabryan@gmail.com>
2015-09-01 20:01:27 +1000

    Converted publishing to use my custom, generic publishing function.

--- publish.el
+++ publish.el
@@ -1,35 +1,5 @@
-(setq pb/geometric_pipe_flows-org-publish-project-alist
-      '(
-	("geometric_pipe_flows-org"
-	 :base-directory "~/working/academic/research/geometric_pipe_flows"
-	 :publishing-directory "~/publish/geometric_pipe_flows"
-	 :base-extension "org"
-	 :recursive t
-	 :publishing-function org-latex-publish-to-latex
-	 :export-with-tags nil
-	 :headline-levels 10
-	 :with-toc nil
-	 :section-numbers t
-					;	:sub-superscript nil
-	 :todo-keywords nil
-	 :author nil
-	 :creator-info nil
-	 :table-auto-headline t
-	 :style-include-default t
-	 :timestamp f
-	 :exclude-tags ("noexport")
-	 :auto-preamble t)
+(setq pb/current-project "geometric_pipe_flows")
+(setq pb/org-src-path  (concat "~/working/academic/research/" pb/current-project))
+(setq pb/org-publish-path (concat "~/publish/" pb/current-project))
 
-	("geometric_pipe_flows-static"
-	 :base-directory "~/working/academic/research/geometric_pipe_flows"
-	 :publishing-directory "~/publish/geometric_pipe_flows"
-	 :recursive t
-	 :base-extension "tex\\|pdf"
-	 :publishing-function org-publish-attachment
-	 )
-
-        ("geometric_pipe_flows" :components ("geometric_pipe_flows-org" "geometric_pipe_flows-static"))
-	))
-
-
-(setq org-publish-project-alist pb/geometric_pipe_flows-org-publish-project-alist)
+(setq org-publish-project-alist (pb/create-org-publish-alist pb/current-project pb/org-src-path pb/org-publish-path))
