1c340a8efdbb4d0c886aeda054e794bca8f71d1a
Paul Bryan <pabryan@gmail.com>
2015-12-16 11:06:45 +0100

    Publish into academic/research directory.

--- publish.el
+++ publish.el
@@ -1,5 +1,5 @@
 (setq pb/current-project "geometric_pipe_flows")
 (setq pb/org-src-path  (concat "~/working/academic/research/" pb/current-project))
-(setq pb/org-publish-path (concat "~/publish/" pb/current-project))
+(setq pb/org-publish-path (concat "~/publish/academic/research/" pb/current-project))
 
 (setq org-publish-project-alist (pb/create-org-publish-alist pb/current-project pb/org-src-path pb/org-publish-path))
